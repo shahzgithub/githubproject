267549361077db0c17a67bf3497bc4e6d150c21e
shahzgithub <143381478+shahzgithub@users.noreply.github.com>
2023-09-11 13:44:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,94 @@
+//chapter1
+//Q1
+alert("firstname:john");
+alert("lastname:smith");
+alert("email:john.smith@example.com");
+alert("phonenumber:(123) 456-7890");
+alert("password:******** (for security reasons, it's not advisable to share actual passwords)!");
+//Q2
+alert("You're learing Javascript!");
+//Q3
+alert("Lorem ipsum, dolor sit amet consectetur adipisicing elit.");
+//chapter2
+//Q1
+alert('myVariableName:JAVASCRIPT!');
+//Q2
+ alert("my_variable");
+ my_variable = ("Hello, world!");
+//Q3
+var teamName;
+teamName = ("shah team");
+alert("Team Name: " + teamName);
+//Q4
+var bestMan=("John");
+alert("bestMan");
+//chapter3
+//Q1
+var caseQty;
+alert("caseQty");
+//Q2
+caseQty = ("144");
+alert("caseQty = 144");
+//Q3
+var num = ("9");
+alert("num = 9");
+//Q4
+var sum;
+sum = 5 + 3 ;
+alert("sum = 5 + 3");
+//Q5
+orderTotal =("merchTotal + shippingCharge"); 
+orderTotal =("100 + 10"); 
+orderTotal =("110"); 
+alert("110");
+//Q6
+var myNumber = ("5");
+myNumber = ("myNumber + 3");
+var res = ("8");
+alert("res = 8");
+//chapter4
+//Q1
+var productCost = ("3.45");
+alert("3.45");
+//Q2
+var nameOfBand;
+alert("nameOfBand");
+//Q3
+var myNumber = 42;
+alert("myNumber");
+//Q4
+var myFullName = ("shahzman");
+alert("shahzman");
+//Q5 
+alert("example of legal: myVariable /n privateVar /n $specialVar /n variable123");
+alert("Example of illegal: 123variable /n my Variable /n if /n @illegal");
+//chapter5
+//Q1
+// 10 % 3;
+var res = ("3.33");
+alert("res = 3.33");
+//Q2
+20 % 6;
+var res = ("2");
+alert("res = 2");
+//Q3
+var largeNum =("1000 * 2000");
+var res =("2,000,000");
+alert("2,000,000");
+//Q4
+("var variable1 = 10");
+("var variable2 = 5");
+("var result = variable1 - variable2");
+alert("5");
+//Q5
+("var dividend = 20");
+("var divisor = 7");
+("var remainder = dividend % divisor");
+alert("2.85");
+//Q6
+("var num1 = 5");
+("var num2 = 10");
+("var result = num1 * num2");
+alert("The result of the multiplication is:50 + result");
+
+
